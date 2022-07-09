e45efb7fc0ff726548e42b8d7a3edd25b9c5a6e0
teegi10 <mateoteggi16@gmail.com>
2022-07-09 18:15:35 -0300

    agrego servidor local de cors

--- src/main/java/com/ligavoley/BackLigaVoleyApplication.java
+++ src/main/java/com/ligavoley/BackLigaVoleyApplication.java
@@ -20,7 +20,7 @@ public class BackLigaVoleyApplication {
 	public CorsFilter corsFilter() {
 	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	corsConfiguration.setAllowCredentials(true);
-	corsConfiguration.setAllowedOrigins(Arrays.asList("https://liga-de-voley.web.app"));
+	corsConfiguration.setAllowedOrigins(Arrays.asList("https://liga-de-voley.web.app","http://localhost:4200"));
 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 	"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 	"Access-Control-Requested-Method", "Access-Control-Allow-Credentials"));
