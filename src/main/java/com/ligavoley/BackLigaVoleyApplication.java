777d2d0de64d3ecaf286c545d51497debb2aef5b
teegi10 <mateoteggi16@gmail.com>
2022-07-29 12:24:07 -0300

    update cors

--- src/main/java/com/ligavoley/BackLigaVoleyApplication.java
+++ src/main/java/com/ligavoley/BackLigaVoleyApplication.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,15 +21,18 @@ public class BackLigaVoleyApplication {
 	public CorsFilter corsFilter() {
 	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	corsConfiguration.setAllowCredentials(true);
-	corsConfiguration.setAllowedOrigins(Arrays.asList("https://liga-de-voley.web.app","http://localhost:4200"));
-	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-	"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+	corsConfiguration.setAllowedOrigins(Arrays.asList("*","https://liga-de-voley.web.app","http://localhost:4200"));
+	corsConfiguration.setAllowedHeaders(Arrays.asList("*","Origin", "Access-Control-Allow-Origin", "Content-Type",
+	"Accept", "Authorization", "Origin","Accept", "X-Requested-With",
 	"Access-Control-Requested-Method", "Access-Control-Allow-Credentials"));
+	corsConfiguration.addAllowedMethod("*");
 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Control-Type", "Accept", "Authorization",
 	"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","OPTIONS"));
 	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+	FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(urlBasedCorsConfigurationSource));
+	bean.setOrder(0);
 	return new CorsFilter ( urlBasedCorsConfigurationSource);
 	}
 
